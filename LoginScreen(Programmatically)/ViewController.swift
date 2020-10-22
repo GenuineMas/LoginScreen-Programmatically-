518ceaecaa25c159ee1858d9536c8ae671130e7a
GenuineMas <MasalitinJW@gmail.com>
2020-10-22 21:18:13 +0300

    Initial Commit

---
+++ LoginScreen(Programmatically)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginScreen(Programmatically)
+//
+//  Created by Genuine on 22.10.2020.
+//  Copyright © 2020 Genuine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
