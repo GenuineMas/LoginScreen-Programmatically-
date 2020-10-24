d5075af2d4458844776b5d853b170fb792b4e81a
GenuineMas <MasalitinJW@gmail.com>
2020-10-24 12:08:58 +0300

    setup scroll view under login view

--- LoginScreen(Programmatically)/LoginController.swift
+++ LoginScreen(Programmatically)/LoginController.swift
@@ -12,26 +12,57 @@ class LoginController: UIViewController {
 
     var loginView: LoginView!
     
+    let scrollView: UIScrollView = {
+        let sv = UIScrollView()
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.alwaysBounceVertical = true
+        sv.backgroundColor = UIColor.blue
+        sv.bounces = false
+        sv.showsVerticalScrollIndicator = false
+        return sv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
         setupView()
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.navigationBar.isHidden = true 
+        navigationController?.navigationBar.isHidden = true
     }
     
     func setupView() {
+        
+        self.view.addSubview(scrollView)
+        
         let mainView = LoginView(frame: self.view.frame)
         self.loginView = mainView
-        self.view.addSubview(loginView)
-        loginView.setAnchor(top: view.topAnchor, paddingTop: 0, bottom: view.bottomAnchor, paddingBottom: 0, left: view.leftAnchor, paddingLeft: 0, right: view.rightAnchor, paddingRight: 0, width: 50, height: 50)
-       
         
+        scrollView.addSubview(loginView)
+        scrollView.setAnchor(top: view.topAnchor, paddingTop: 0, bottom: view.bottomAnchor, paddingBottom: 0, left: view.leftAnchor, paddingLeft: 0, right: view.rightAnchor, paddingRight: 0, width: 50, height: 50)
+        mainView.setAnchor(top: view.topAnchor, paddingTop: 0, bottom: view.bottomAnchor, paddingBottom: 0, left: view.leftAnchor, paddingLeft: 0, right: view.rightAnchor, paddingRight: 0, width: 50, height: 50)
+       
     }
-
+    
+    
+    @objc func keyboardWillShow(notification: NSNotification ){
+        guard let userInfo = notification.userInfo else {return}
+        guard let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {return}
+        let keyboardFrame = keyboardSize.cgRectValue
+        if self.view.frame.origin.y == 0 {
+            self.view.frame.origin.y -= keyboardFrame.height
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification ){
+        if self.view.frame.origin.y != 0 {
+                self.view.frame.origin.y = 0
+            }
+    }
 
 }
 
--- LoginScreen(Programmatically)/LoginView.swift
+++ LoginScreen(Programmatically)/LoginView.swift
@@ -19,11 +19,26 @@ class LoginView: UIView {
         let stackView = mainStackView()
         addSubview(stackView)
         backgroundColor = .darkGray
-        stackView.setAnchor(width: self.frame.width - 60, height: 210)
+        stackView.setAnchor(width: self.frame.width - 60, height: 300)
         stackView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         stackView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive =  true
       }
     
+    let nameTextField: UITextField = {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.layer.cornerRadius = 5
+        tf.backgroundColor = UIColor(red: 216/255, green: 216/255, blue: 216/255, alpha: 0.2)
+        tf.textColor = UIColor(white: 0.9, alpha: 0.8)
+        tf.font = UIFont.systemFont(ofSize: 17)
+        tf.autocorrectionType = .no
+        //placeholder
+        var placeholder = NSMutableAttributedString()
+        placeholder = NSMutableAttributedString(attributedString: NSAttributedString(string: "Enter your name", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 18), .foregroundColor:UIColor(white: 1, alpha: 0.7) ]))
+        tf.attributedPlaceholder = placeholder
+        tf.setAnchor(width: 0, height: 40)
+        return tf
+    }()
 
     let emailTextField: UITextField = {
         let tf = UITextField()
@@ -57,6 +72,22 @@ class LoginView: UIView {
         return tf
     }()
     
+    let confirmPasswordTextField: UITextField = {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.layer.cornerRadius = 5
+        tf.backgroundColor = UIColor(red: 216/255, green: 216/255, blue: 216/255, alpha: 0.2)
+        tf.textColor = UIColor(white: 0.9, alpha: 0.8)
+        tf.font = UIFont.systemFont(ofSize: 17)
+        tf.autocorrectionType = .no
+        //placeholder
+        var placeholder = NSMutableAttributedString()
+        placeholder = NSMutableAttributedString(attributedString: NSAttributedString(string: "Confirm password", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 18), .foregroundColor:UIColor(white: 1, alpha: 0.7) ]))
+        tf.attributedPlaceholder = placeholder
+        tf.setAnchor(width: 0, height: 40)
+        return tf
+    }()
+    
     let loginButton: UIButton = {
         let button = UIButton()
         let attributedString = NSMutableAttributedString(attributedString: NSAttributedString(string: "Login", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 18), .foregroundColor: UIColor.white]))
@@ -64,14 +95,17 @@ class LoginView: UIView {
         button.layer.cornerRadius = 5
         button.layer.borderWidth = 1
         button.layer.borderColor = UIColor(red: 40/255, green: 151/255, blue: 164/255, alpha: 1).cgColor
-        button.setAnchor(width: 0, height: 50)
+        button.setAnchor(width: 0, height: 40)
         return button
     }()
     
+    //Putting all views in stackView for easy adding constraints
     func mainStackView() -> UIStackView {
-        let stackView = UIStackView(arrangedSubviews: [emailTextField,passwordTextField,loginButton])
+        
+        let stackView = UIStackView(arrangedSubviews: [nameTextField,emailTextField, passwordTextField,confirmPasswordTextField, loginButton])
+      //  stackView.translatesAutoresizingMaskIntoConstraints = false  /////
         stackView.axis = .vertical
-        stackView.distribution = .fillProportionally
+        stackView.distribution = .fillEqually
         stackView.spacing = 10
         return stackView
     }
